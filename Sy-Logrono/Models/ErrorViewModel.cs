a8a4434a010fc0ce09d5adc9ff2cd77e5028700b
Eisen Lewis Sy <eisenlewis.sy@benilde.edu.ph>
2022-07-25 11:06:09 +0800

    Add project files.

---
+++ Sy-Logrono/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sy_Logrono.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
